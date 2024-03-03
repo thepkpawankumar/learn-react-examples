7a95813a0c00f95e36aaf718e253e27ed0596bd5
Pawan <thepkpawankumar@gmail.com>
2024-03-03 12:41:43 +0530

    Implemented increment count example

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ function App() {
 
   /**
    * Using const with useState will not work in case of count++ or ++count
+   * As count++ and ++count assign value back to variable and since count is constant
+   * so it will throw an error "Assignment to constant variable"
+   * If you change const to let then it will work but in case of count++
+   * Counter will be updated by clicking twice on the button
+   * As count++ will first assign the value to count, since initially count is 0 so
+   * it will assign 0 to state and then increment the count, so state doesn't change and on next click
+   * since count is 1 now so 1 is assigned to state and then it is incremented by 1, hence it took 2 click
+   * to update the actual value, we can use ++count to resolve it
+   * but recommended way is we should use const for state initialization and then use count+1 to increment the actual counter
    * 
    */
   return (
     <div className="App">
      
-     <button onClick={() => {setCount(++count)}}>Click Me</button>
+     <button onClick={() => {setCount(count+1)}}>Click Me</button>
      <p>Count {count}</p>
     </div>
   );
